b3396d6f6ef49682e6fc9a2a13a6cb91a66de5cf
aldenweaver <aldenweavercodes@gmail.com>
2020-01-31 15:18:49 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,29 +1,30 @@
+require 'pry'
 class Application
- 
-    @@items = ["Apples","Carrots","Pears"]
-   
     def call(env)
       resp = Rack::Response.new
       req = Rack::Request.new(env)
    
-      if req.path.match(/items/)
-        @@items.each do |item|
-          resp.write "#{item}\n"
-        end
-      elsif req.path.match(/search/)
-   
-        search_term = req.params["q"]
-   
-        if @@items.include?(search_term)
-          resp.write "#{search_term} is one of our items"
-        else
-          resp.write "Couldn't find #{search_term}"
-        end
-   
+      # if req.path.match(/items/)
+      #   @@items.each do |item|
+      #     resp.write "#{item}\n"
+      #   end
+    items = Item.all 
+
+    if req.path.match(/items/)
+      item_title = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item = items.find{|s| s.name.downcase == item_title.downcase}
+      # binding.pry
+      if item.nil?
+        resp.write "Item not found"
+        resp.status = 400
       else
-        resp.write "Path Not Found"
+        resp.write item.price
       end
-   
-      resp.finish
+    else
+      resp.write "Route not found"
+      resp.status = 404
     end
+  
+    resp.finish
   end
+end
